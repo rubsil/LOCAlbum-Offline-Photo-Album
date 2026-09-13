9d7440a6d1b5d0f1449cc9dd645a31a8194d5cb4
Rúben Silva <128933310+rubsil@users.noreply.github.com>
2026-09-13 22:49:54 +0000

    Update LOCALBUM - Manager.bat

--- Album/LOCALBUM - Manager.bat
+++ Album/LOCALBUM - Manager.bat
@@ -20,7 +20,7 @@ for %%I in ("%ROOT%\..") do set "SOURCE_DIR=%%~fI"
 :: =====================================================
 ::  OCULTAR ficheiros técnicos logo ao arrancar
 :: =====================================================
-for %%A in (z1.ps1 z3.ps1 z-backup.ps1 template.html favicon.png ajuda_album.png exiftool.exe ffmpeg.exe) do (
+for %%A in (z1.ps1 z3.ps1 z-backup.ps1 template.html favicon.png ajuda_album.png exiftool.exe exiftool_files exiftool_files.zip ffmpeg.exe) do (
     if exist "%ROOT%\%%A" (
         attrib +h +s "%ROOT%\%%A" >nul 2>&1
     )
